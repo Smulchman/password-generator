9c6650fb1f42f10f5e65bbe68abbd657329286d4
smulch <mulcahy.samuel@gmail.com>
2023-02-08 19:10:45 -0500

    added character arrays

--- Develop/script.js
+++ Develop/script.js
@@ -22,4 +22,11 @@ generateBtn.addEventListener("click", writePassword);
 // if theyre stored in an array, turn the array to a string
 // print the generated string in the right place
 
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+var numbers = ['1','2','3','4','5','6','7','8','9','0'];
+
+var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-", ".", "`", "~", "|", "<", ">", "=", "-", "_"];
 
