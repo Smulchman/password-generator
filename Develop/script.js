d0e6ec5078bdbc360cd9edd66b1522d9c1b7359b
smulch <mulcahy.samuel@gmail.com>
2023-02-08 19:04:02 -0500

    added pseudocode

--- Develop/script.js
+++ Develop/script.js
@@ -12,3 +12,14 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// pseudocode
+// I want to make arrays with all characters
+// as the user decides what they want
+// combine the arrays into a temp local variable
+// generate a random number the characters desired amt of times
+// add those to another array maybe or a string
+// if theyre stored in an array, turn the array to a string
+// print the generated string in the right place
+
+
